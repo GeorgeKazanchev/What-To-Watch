c65d52f5dcaf17b103de27a83a0268acdb48024f
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2023-12-20 13:53:24 +0300

    Выполнена первоначальная настройка проекта
    1. Добавлены стили, картинки, index.html и файл манифеста.
    
    2. Настроены сборщик модулей (Webpack), линтер (ESLint), компилятор Babel.
    
    3. Определены зависимости в package.json.
    
    4. Добавлены файлы для git: .gitignore и .gitattributes.
    
    5. Добавлены файл с настройками для текстовых редакторов и конфигурационный файл Travis CI (планируется использование данного сервиса в будущем).

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const webpackConfig = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, "public")
+    },
+    devServer: {
+        static: path.join(__dirname, "public"),
+        open: true,
+        port: 44402,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            }
+        ],
+    },
+    devtool: "source-map",
+    mode: "production"
+};
+
+export default webpackConfig;
