e5660f5fea0e36b2e673339817df6d0c404285e9
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-01-18 15:27:14 +0300

    Расширены данные для отладки

--- src/debug-data/debug-data.js
+++ src/debug-data/debug-data.js
@@ -9,6 +9,7 @@ export const promoMovie = {
     ' who tells him of his early years serving as a lobby boy in the hotel\'s' + 
     ' glorious years under an exceptional concierge.',
     rating: '88',
+    ratingDescription: 'Excellent',
     reviewsCount: 1105,
     director: 'Wes Anderson',
     starring: ['Ralph Fiennes', 'F. Murray Abraham', 'Mathieu Amalric', 'Adrien Brody',
@@ -77,6 +78,7 @@ export const movies = [
         description: 'The second installment of the "Fantastic Beasts" series ' + 
         'featuring the adventures of Magizoologist Newt Scamander.',
         rating: '52',
+        ratingDescription: 'Good',
         reviewsCount: 1644,
         director: 'David Yates',
         starring: []
@@ -90,6 +92,7 @@ export const movies = [
         description: 'The story of the legendary British rock band Queen and lead singer Freddie Mercury, ' + 
         'leading up to their famous performance at Live Aid (1985).',
         rating: '49',
+        ratingDescription: 'Bad',
         reviewsCount: 3565,
         director: 'Bryan Singer',
         starring: []
@@ -104,6 +107,7 @@ export const movies = [
         'one day he will become King of Scotland. Consumed by ambition and spurred to action by his wife, ' + 
         'Macbeth murders his king and takes the throne for himself.',
         rating: '71',
+        ratingDescription: 'Good',
         reviewsCount: 280,
         director: 'Justin Kurzel',
         starring: []
@@ -117,6 +121,7 @@ export const movies = [
         description: 'A biopic depicting the early years of legendary director and aviator Howard Hughes\' ' + 
         'career from the late 1920s to the mid 1940s.',
         rating: '77',
+        ratingDescription: 'Excellent',
         reviewsCount: 907,
         director: 'Martin Scorsese',
         starring: []
@@ -131,6 +136,7 @@ export const movies = [
         'things he says and does as he grows up. But Kevin is just getting started, and his final act will ' + 
         'be beyond anything anyone imagined.',
         rating: '68',
+        ratingDescription: 'Good',
         reviewsCount: 522,
         director: 'Lynne Ramsay',
         starring: []
@@ -143,6 +149,7 @@ export const movies = [
         poster: 'img/what-we-do-in-the-shadows.jpg',
         description: 'A look into the nightly lives of four vampires who have lived together on Staten Island for over a century.',
         rating: '',
+        ratingDescription: '',
         reviewsCount: 492,
         director: 'Jemaine Clement',
         starring: []
@@ -151,11 +158,12 @@ export const movies = [
         id: 'a3cf4dcf-54be-489e-b672-ed2e3115e282',
         title: 'The Revenant',
         genre: 'Drama',
-        year: '',
+        year: '2015',
         poster: 'img/revenant.jpg',
         description: 'A frontiersman on a fur trading expedition in the 1820s fights for survival after being mauled ' + 
         'by a bear and left for dead by members of his own hunting team.',
         rating: '76',
+        ratingDescription: 'Excellent',
         reviewsCount: 1872,
         director: 'Alejandro G. Iñárritu',
         starring: []
@@ -169,6 +177,7 @@ export const movies = [
         description: 'Teddy Daniels and Chuck Aule, two US marshals, are sent to an asylum on a remote island in order ' + 
         'to investigate the disappearance of a patient, where Teddy uncovers a shocking truth about the place.',
         rating: '63',
+        ratingDescription: 'Good',
         reviewsCount: 1649,
         director: 'Martin Scorsese',
         starring: []
@@ -182,6 +191,7 @@ export const movies = [
         description: 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner ' + 
         'bandits intertwine in four tales of violence and redemption.',
         rating: '95',
+        ratingDescription: 'Masterpiece',
         reviewsCount: 3664,
         director: 'Quentin Tarantino',
         starring: []
@@ -196,6 +206,7 @@ export const movies = [
         'he cannot resist the cash left behind and takes it. The hunter becomes the hunted when a merciless killer ' + 
         'named Chigurh picks up his trail.',
         rating: '92',
+        ratingDescription: 'Excellent',
         reviewsCount: 2143,
         director: 'Coen brothers',
         starring: []
@@ -209,8 +220,123 @@ export const movies = [
         description: 'An alien invasion threatens the future of humanity. The catastrophic nightmare is depicted ' + 
         'through the eyes of one American family fighting for survival.',
         rating: '73',
+        ratingDescription: 'Good',
         reviewsCount: 3002,
         director: 'Steven Spielberg',
         starring: []
+    },
+    {
+        id: '868a2461-2081-4da3-b586-0262dbc8c94a',
+        title: 'Johnny English',
+        genre: 'Comedy',
+        year: '2003',
+        poster: 'img/johnny-english.jpg',
+        description: 'After a sudden attack on MI5, Johnny English, Britain\'s most confident, yet unintelligent ' + 
+        'spy, becomes Britain\'s only spy.',
+        rating: '51',
+        ratingDescription: 'Good',
+        reviewsCount: 352,
+        director: 'Peter Howitt',
+        starring: []
+    },
+    {
+        id: 'ecc57abe-684e-43d1-a754-7c6596686ffe',
+        title: 'Snatch',
+        genre: 'Comedy',
+        year: '2000',
+        poster: 'img/snatch.jpg',
+        description: 'Unscrupulous boxing promoters, violent bookmakers, a Russian gangster, incompetent amateur ' + 
+        'robbers and supposedly Jewish jewelers fight to track down a priceless stolen diamond.',
+        rating: '55',
+        ratingDescription: 'Good',
+        reviewsCount: 945,
+        director: 'Guy Ritchie',
+        starring: []
+    },
+    {
+        id: '3adc0faa-6c83-4490-bb8c-373fff32c456',
+        title: 'Moonrise Kingdom',
+        genre: 'Adventure',
+        year: '2012',
+        poster: 'img/moonrise-kingdom.jpg',
+        description: 'Two 12-year-olds, who live on an island, fall in love with each other and elope into the ' + 
+        'wilderness. While people set out on a search mission, a violent storm approaching them catches their attention.',
+        rating: '84',
+        ratingDescription: 'Excellent',
+        reviewsCount: 510,
+        director: 'Wes Anderson',
+        starring: []
+    },
+    {
+        id: '7a9da465-9f8a-402d-ac4c-6fd0ffd2ab47',
+        title: 'Seven Years in Tibet',
+        genre: 'Adventure',
+        year: '1997',
+        poster: 'img/seven-years-in-tibet.jpg',
+        description: 'True story of Heinrich Harrer, an Austrian mountain climber who became friends with ' + 
+        'the Dalai Lama at the time of China\'s takeover of Tibet.',
+        rating: '55',
+        ratingDescription: 'Good',
+        reviewsCount: 191,
+        director: 'Jean-Jacques Annaud',
+        starring: []
+    },
+    {
+        id: 'bbc28343-e273-4db5-b6a6-db19864c5a13',
+        title: 'Midnight Special',
+        genre: 'Sci-Fi',
+        year: '2016',
+        poster: 'img/midnight-special.jpg',
+        description: 'A father and son go on the run, pursued by the government and a cult drawn to the ' + 
+        'child\'s special powers.',
+        rating: '76',
+        ratingDescription: 'Excellent',
+        reviewsCount: 340,
+        director: 'Jeff Nichols',
+        starring: []
+    },
+    {
+        id: 'f74819d9-300e-4323-a48a-762a9f99f9f8',
+        title: 'The Dardjeeling Limited',
+        genre: 'Adventure',
+        year: '2007',
+        poster: 'img/dardjeeling-limited.jpg',
+        description: 'A year after their father\'s funeral, three brothers travel across India by train ' + 
+        'in an attempt to bond with each other.',
+        rating: '67',
+        ratingDescription: 'Good',
+        reviewsCount: 361,
+        director: 'Wes Anderson',
+        starring: []
+    },
+    {
+        id: 'f15b0001-beea-435a-b4b7-6d20df54d228',
+        title: 'Orlando',
+        genre: 'Drama',
+        year: '1992',
+        poster: 'img/orlando.jpg',
+        description: 'After Queen Elizabeth I commands him not to grow old, a young nobleman struggles ' + 
+        'with love and his place in the world.',
+        rating: '75',
+        ratingDescription: 'Excellent',
+        reviewsCount: 102,
+        director: 'Sally Potter',
+        starring: []
+    },
+    {
+        id: 'cf28f87b-59b9-477f-bca8-a5d3b40db2c8',
+        title: 'Mindhunter',
+        genre: 'Drama',
+        year: '2017-2019',
+        poster: 'img/mindhunter.jpg',
+        description: 'In the late 1970s, two FBI agents broaden the realm of criminal science by ' + 
+        'investigating the psychology behind murder and end up getting too close to real-life monsters.',
+        rating: '',
+        ratingDescription: '',
+        reviewsCount: 988,
+        director: 'Joe Penhall',
+        starring: []
     }
 ];
+
+export const similarMovies = movies.slice(0, 4);
