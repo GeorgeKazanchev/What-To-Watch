71ce87b137241c54de45b73b7433e4ea37c67428
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-02-09 20:05:46 +0300

    Удалены более не используемые файлы
    Причиной удаления является перенос файлов в папки с конкретными модулями.

--- src/util/constants.js
+++
@@ -1,8 +0,0 @@
-export const NavTabs = {
-    OVERVIEW: 'Overview',
-    DETAILS: 'Details',
-    REVIEWS: 'Reviews'
-};
-
-export const MAX_SIMILAR_MOVIES_COUNT = 4;
-export const VIDEO_NOT_EXIST_ERROR_MESSAGE = 'Your browser does not support the video tag.';
