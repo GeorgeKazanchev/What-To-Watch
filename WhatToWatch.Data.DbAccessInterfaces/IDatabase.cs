aaffd65430dcdfe524039a9cc33ff6ed04d45789
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-02-10 16:03:47 +0300

    Добавлен интерфейс для представления БД как отдельной сущности

---
+++ WhatToWatch.Data.DbAccessInterfaces/IDatabase.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace WhatToWatch.Data.DbAccessInterfaces
+{
+    public interface IDatabase
+    {
+        public string DatabaseName { get; }
+        public string SchemeName { get; }
+        public IPAddress ServerIpAddress { get; }
+        public int Port { get; }
+        public IPEndPoint ServerEndpoint { get; }
+        public DatabaseType DatabaseType { get; }
+        public string DefaultUsername { get; }
+        public string DefaultPassword { get; }
+        public string ConnectionStringTemplate { get; }
+        public string DefaultConnectionString { get; }
+        public string GetConnectionString(string username, string password);
+    }
+}
