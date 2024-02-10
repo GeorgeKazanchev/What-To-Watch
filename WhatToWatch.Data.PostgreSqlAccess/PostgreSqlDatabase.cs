0f8285a5da2cf5a4b81f0d0c414690b7a14cf422
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-02-10 16:06:19 +0300

    Добавлен класс для представления БД PostgreSQL в виде отдельной сущности

---
+++ WhatToWatch.Data.PostgreSqlAccess/PostgreSqlDatabase.cs
@@ -0,0 +1,55 @@
+﻿using System.Net;
+using WhatToWatch.Data.DbAccessInterfaces;
+
+namespace WhatToWatch.Data.PostgreSqlAccess
+{
+    public class PostgreSqlDatabase : IDatabase
+    {
+        public PostgreSqlDatabase(string databaseName, string schemeName, IPAddress serverIpAddress, int port,
+            DatabaseType databaseType, string defaultUsername, string defaultPassword) 
+            : this(databaseName, schemeName, serverIpAddress, port, databaseType)
+        {
+            DefaultUsername = defaultUsername;
+            DefaultPassword = defaultPassword;
+        }
+
+        public PostgreSqlDatabase(string databaseName, string schemeName, IPAddress serverIpAddress, int port,
+            DatabaseType databaseType)
+        {
+            DatabaseName = databaseName;
+            SchemeName = schemeName;
+            ServerIpAddress = serverIpAddress;
+            Port = port;
+            ServerEndpoint = new IPEndPoint(serverIpAddress, port);
+            DatabaseType = databaseType;
+            DefaultUsername = string.Empty;
+            DefaultPassword = string.Empty;
+        }
+
+        public string DatabaseName { get; }
+        
+        public string SchemeName { get; }
+        
+        public IPAddress ServerIpAddress { get; }
+        
+        public int Port { get; }
+        
+        public IPEndPoint ServerEndpoint { get; }
+        
+        public DatabaseType DatabaseType { get; }
+        
+        public string DefaultUsername { get; }
+        
+        public string DefaultPassword { get; }
+        
+        public string ConnectionStringTemplate =>
+            $"Host={ServerEndpoint.Address};Port={ServerEndpoint.Port};Database={DatabaseName};Search Path={SchemeName};";
+
+        public string DefaultConnectionString => GetConnectionString(DefaultUsername, DefaultPassword);
+        
+        public string GetConnectionString(string username, string password)
+        {
+            return $"{ConnectionStringTemplate}User ID={username};Password={password};";
+        }
+    }
+}
