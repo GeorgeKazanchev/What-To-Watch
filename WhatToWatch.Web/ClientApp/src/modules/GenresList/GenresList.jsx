3905698fc732183af61ffdf3dac1f091f57e6553
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-02-19 21:09:54 +0300

    Добавлено ограничение на максимальное количество отображаемых жанров

--- WhatToWatch.Web/ClientApp/src/modules/GenresList/GenresList.jsx
+++ WhatToWatch.Web/ClientApp/src/modules/GenresList/GenresList.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import getGenresComponents from './helpers/getGenresComponents.jsx';
+import { MAX_SHOWN_GENRES } from './constants/maxShownGenres.js';
 import { CustomPropTypes } from '../../util/custom-prop-types.js';
 
 export default function GenresList({ genres }) {
     const [activeGenre, setActiveGenre] = React.useState(genres[0]);
+    
+    genres = genres.slice(0, MAX_SHOWN_GENRES);
     const genresComponents = getGenresComponents(genres, activeGenre, setActiveGenre);
     
     return (
